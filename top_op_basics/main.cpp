5be4ec07d2d423dfd510f24fb90a2021a63f5e63
Gleb <tsekulingleb2002@gmail.com>
2026-07-08 17:21:29 +0300

    Update project

--- top_op_basics/main.cpp
+++ top_op_basics/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <Eigen/Sparse>
+#include <Eigen/SparseLU>
 #include <math.h>
 using namespace Eigen;
 using namespace std;
@@ -23,36 +23,125 @@ vector<vector<double>> lk()
     return KE;
 }
 
-double FE(int nelx, int nely, const vector<vector<double>>& x, double penal)
+vector<double> solveLinearSystem(vector<vector<double>> A, vector<double> b)
 {
-    int ndof = 2* (nelx+1)* (nely+1);
-    vector<vector<double>> KE=lk();
+    int n = b.size();
 
-    SparseMatrix<double> K(ndof, ndof);
-    SparseMatrix<double> F(ndof, 1);
-    SparseMatrix<double> U(ndof, 1);
+    for (int k = 0; k < n; k++) {
+        int pivot = k;
 
-    vector<Triplet<double>> triplets;
-    
-    for (int ely=1; ely<=nely; ely++)
-    {
-        for (int elx=1; elx<=nelx; elx++)
-        {
-            int n1 = (nely+1)*(elx-1)+ely;
-            int n2 = (nely+1)*elx+ely;
-            vector<int> edof ={2*n1-1, 2*n1, 2*n2-1, 2*n2, 2*n2+1, 2*n2+2, 2*n1+1, 2*n1+2};
+        for (int i = k + 1; i < n; i++) {
+            if (abs(A[i][k]) > abs(A[pivot][k])) {
+                pivot = i;
+            }
+        }
 
-            for (int i = 0; i < 8; i++) 
-            {
-                for (int j = 0; j < 8; j++) 
-                {
-                    triplets.push_back(
-                        Triplet<double>(edof[i] - 1, edof[j] - 1, pow(x[ely - 1][elx - 1], penal) * KE[i][j]));
+        swap(A[k], A[pivot]);
+        swap(b[k], b[pivot]);
+
+        for (int i = k + 1; i < n; i++) {
+            double factor = A[i][k] / A[k][k];
+
+            for (int j = k; j < n; j++) {
+                A[i][j] -= factor * A[k][j];
+            }
+
+            b[i] -= factor * b[k];
+        }
+    }
+
+    vector<double> x(n, 0.0);
+
+    for (int i = n - 1; i >= 0; i--) {
+        double sum = b[i];
+
+        for (int j = i + 1; j < n; j++) {
+            sum -= A[i][j] * x[j];
+        }
+
+        x[i] = sum / A[i][i];
+    }
+
+    return x;
+}
+
+vector<double> FE(int nelx, int nely, const vector<vector<double>>& x, double penal)
+{
+    int ndof = 2 * (nelx + 1) * (nely + 1);
+
+    vector<vector<double>> KE = lk();
+
+    vector<vector<double>> K(ndof, vector<double>(ndof, 0.0));
+    vector<double> F(ndof, 0.0);
+    vector<double> U(ndof, 0.0);
+
+    for (int ely = 1; ely <= nely; ely++) {
+        for (int elx = 1; elx <= nelx; elx++) {
+            int n1 = (nely + 1) * (elx - 1) + ely;
+            int n2 = (nely + 1) * elx + ely;
+
+            vector<int> edof = {2*n1-1, 2*n1, 2*n2-1, 2*n2, 2*n2+1, 2*n2+2, 2*n1+1, 2*n1+2};
+
+            double factor = pow(x[ely - 1][elx - 1], penal);
+
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    K[edof[i] - 1][edof[j] - 1] += factor * KE[i][j];
                 }
             }
         }
     }
-    return 0;
+
+    F[1] = -1.0;
+
+    vector<int> fixeddofs;
+
+    for (int dof = 0; dof <= 2 * (nely + 1) - 1; dof += 2) {
+        fixeddofs.push_back(dof);
+    }
+
+    fixeddofs.push_back(ndof - 1);
+
+    sort(fixeddofs.begin(), fixeddofs.end());
+    fixeddofs.erase(unique(fixeddofs.begin(), fixeddofs.end()), fixeddofs.end());
+
+    vector<int> freedofs;
+
+    for (int dof = 0; dof < ndof; dof++) {
+        if (!binary_search(fixeddofs.begin(), fixeddofs.end(), dof)) {
+            freedofs.push_back(dof);
+        }
+    }
+
+    int nfree = freedofs.size();
+
+    vector<vector<double>> Kff(nfree, vector<double>(nfree, 0.0));
+    vector<double> Ff(nfree, 0.0);
+
+    for (int i = 0; i < nfree; i++) {
+        Ff[i] = F[freedofs[i]];
+
+        for (int j = 0; j < nfree; j++) {
+            Kff[i][j] = K[freedofs[i]][freedofs[j]];
+        }
+    }
+
+    vector<double> Uf = solveLinearSystem(Kff, Ff);
+
+    for (int i = 0; i < nfree; i++) {
+        U[freedofs[i]] = Uf[i];
+    }
+
+    for (int i = 0; i < fixeddofs.size(); i++) {
+        U[fixeddofs[i]] = 0.0;
+    }
+
+    return U;
+}
+
+double check(int nelx, int nely, double rmin, const vector<vector<double>>& x)
+{
+
 }
 
 int main()
@@ -71,6 +160,22 @@ int main()
     while (change > 0.01)
     {
         loop++;
+        xold=x;
+        vector <double> U=FE(nelx, nely, x, penal);
+        vector<vector<double>> KE =lk();
+        vector<vector<double>> dc(nely, vector<double>(nelx, 0.0));
+         double c=0.;
+         for (int ely=1;ely<=nely;ely++)
+         {
+            for (int elx=1; elx<=nelx;elx++)
+            {
+                int n1=(nely+1)*(elx-1)+ely;
+                int n2=(nely+1)*elx + ely;
+                vector<int> edof = {2*n1-1, 2*n1, 2*n2-1, 2*n2, 2*n2+1, 2*n2+2, 2*n1+1, 2*n1+2};
+                vector<double> Ue = U{2*n1-1, 2*n1, 2*n2-1, 2*n2, 2*n2+1, 2*n2+2, 2*n1+1, 2*n1+2};
+                double c = pow(x(ely,elx),penal-1)*Ue'*KE*Ue;
+            }
+         }
     }
 
     return 0;
