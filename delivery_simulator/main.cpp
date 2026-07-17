4702e3df6466130b28235cef39ea5d724ddc1959
Gleb <tsekulingleb2002@gmail.com>
2026-07-17 14:07:37 +0300

    step 1. packages

--- delivery_simulator/main.cpp
+++ delivery_simulator/main.cpp
@@ -1,8 +1,62 @@
 #include <iostream>
 using namespace std;
 
+class Package
+{
+    int id;
+    double weight;
+    string status, name;
+    static int num;
+public:
+    Package(string const  &name, double weight)
+    {
+        if (weight <0)
+        {
+            cout <<"Incorrect weight value, pls enter correct num"<<endl;
+        }
+        else 
+        {
+            this->weight=weight;
+        }
+            this->name=name;
+            status="Created";
+            id=++num;
+    }
+    void send()
+    {
+        status="InTransit";
+    };
+    void deliver()
+    {
+        status="Delivered";
+    };
+    void PrintInfo()
+    {
+        cout<<"Number of packages: "<< num<<"\nID: "<<id<<"\nName: "<<name<<"\nWeight of packages, kg: "<<weight<<"\nStatus: "<<status<<endl;
+        cout <<"_______________________________________"<<endl;
+    };
+    int GetID()
+    {
+        return num;
+    };
+};
+
+int Package::num =0;
+
+
 int main()
 {
-    cout<< "Hello, world!";
+    Package package("Ann",2.5);
+    Package package2("Max",123);
+    package.PrintInfo();
+    package2.PrintInfo();
+    package.GetID();
+    package2.GetID();
+    package.send();
+    package2.send();
+    package.deliver();
+    package2.deliver();
+    package.PrintInfo();
+    package2.PrintInfo();
     return 0;
 }
