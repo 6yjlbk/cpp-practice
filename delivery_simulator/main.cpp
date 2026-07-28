e43c97531e33e328d6157b885aa9abf4fb90872d
Gleb <tsekulingleb2002@gmail.com>
2026-07-28 19:35:31 +0300

    update stage 3

--- delivery_simulator/main.cpp
+++ delivery_simulator/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Package
@@ -12,7 +13,9 @@ public:
     {
         if (weight <0)
         {
-            cout <<"Incorrect weight value, pls enter correct num"<<endl;
+            throw invalid_argument(
+            "Weight must be greater than zero"
+        );
         }
         else 
         {
@@ -37,7 +40,7 @@ public:
     };
     int GetID()
     {
-        return num;
+        return id;
     };
     virtual double cost() =0;
 };
@@ -47,16 +50,7 @@ public:
     using Package::Package;
     double cost() override
     {
-        if (weight <0)
-        {
-            cout <<"Incorrect weight value, pls enter correct num"<<endl;
-        }
-        else 
-        {
-            this->weight=weight;
-        }
         price=weight*100;
-        return price;
     };
 };
 
@@ -65,14 +59,6 @@ class ExpressPackage: public Package{
 public:
     ExpressPackage(const string &name, double weight, double time):Package(name,weight),time(time)
     {
-        if (weight <0)
-        {
-            cout <<"Incorrect weight value, pls enter correct num"<<endl;
-        }
-        else 
-        {
-            this->weight=weight;
-        }
     }
     double cost() override
     {
@@ -86,14 +72,6 @@ class FragilePackage: public Package{
 public:
     FragilePackage(const string &name, double weight, double lvl_fragility):Package(name,weight),lvl_fragility(lvl_fragility)
     {
-        if (weight <0)
-        {
-            cout <<"Incorrect weight value, pls enter correct num"<<endl;
-        }
-        else 
-        {
-            this->weight=weight;
-        }
     }
     double cost() override
     {
@@ -102,6 +80,98 @@ public:
     };
 };
 
+
+class Courier{
+    public:
+    virtual bool canDeliver (const Package& package)  = 0;
+    virtual double calculateAward (const Package& package)  = 0;
+    virtual string getType() = 0;
+};
+
+class WalkingCourier: public Courier{
+    using Courier::Courier;
+    bool canDeliver (const Package& package) override
+        {
+            if (package.weight <5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    double calculateAward (const Package& package) override
+    {
+        return package.weight*0.1;
+    }
+
+};
+class BikeCourier: public Courier{
+    using Courier::Courier;
+    bool canDeliver (const Package& package) override
+        {
+            if (package.weight <15)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    double calculateAward (const Package& package) override
+    {
+        return package.price*0.2;
+    }
+    
+};
+class CarCourier: public Courier{
+    using Courier::Courier;
+    bool canDeliver (const Package& package)
+    {
+        return true;
+    }
+    double calculateAward (const Package& package) override
+    {
+        return package.price*0.3;
+    }
+    
+};
+
+class DeliveryService:public Package, Courier{
+    Package* package =nullptr;
+    Courier* courier =nullptr;
+    public:
+    void AddPack(Package& package){
+        this -> package=&package;
+    };
+    void RegPack() {
+
+    };
+    void FindPack() {
+
+    };
+    void SetCourier(Courier& courier) {
+        this -> courier=&courier;
+    };
+    void GetAllDel() {
+
+    };
+    double CalcCost() {
+
+    };
+    double CalcAward() {
+
+    };
+    void PrintPackage (){
+        if (package != nullptr)
+        {
+            package->PrintInfo();
+        }
+
+    }
+};
 
 int Package::num =0;
 
